5c2c0a6b7c0353655083f84670d5be45ac8eda3c
alan-va <github@abrahao.proinbox.com>
2020-02-27 17:33:40 -0300

    mudancas na funcao principal

--- factorization.c
+++ factorization.c
@@ -5,7 +5,7 @@
 #include <math.h>
 #include <time.h>
 
-#define maiorInteiro LONG_MAX /* long_max veio da biblioteca <limits.h> */
+#define maiorInteiro LONG_MAX
 
 // variáveis globais
 long int AuxN;
@@ -54,24 +54,29 @@ static int fatoraInteiros(long int Num)
     }
   }
 
-  return 0; /* sem erros */
+  return 0;
 } /* fim - fatoraInteiros */
 
 int main(void)
 {
   long int N;
   float tempoExec;
+  clock_t tic, tac;
 
-  printf("Decomposição de inteiros em fatores primos --- Versão C\nDado N, este programa determina:\n\n");
-  printf("Cada fator pi (i=1, ... , n) de N e ki, a maior potência de pi que divide N; ou seja\n");
-  printf("p1^k1, p2^k2, ... , pn^kn tais que N = (p1^k1) * (p2^k2) * ... * (pn^kn).\n\n");
-  printf("[-1] Maior inteiro representável | [-2] Sair do programa\n\n");
+  printf("%s\n%s\n\n%s\n%s\n\n%s\n\n", 
+    "Decomposição de inteiros em fatores primos",
+    "Dado N, este programa determina:",
+  "Cada fator pi (i=1, ... , n) de N e ki (multiplicidade do fator pi), i.e.,",
+  "p1^k1, p2^k2, ... , pn^kn tais que N = (p1^k1) * (p2^k2) * ... * (pn^kn).",
+  "[-1] Maior inteiro representável | [-2] Sair do programa");
   
   while (true)
   {
-    printf("<Insira N> "); scanf("%ld", &N); AuxN = N; numeroPrimo = true;
+    printf("<Insira N> "); scanf("%ld", &N);
+    AuxN = N; numeroPrimo = true;
 
-    if (N == -1) { printf("Maior inteiro representável: %ld\n\n", maiorInteiro); }
+    if (N == -1)
+      printf("Maior inteiro representável: %ld\n\n", maiorInteiro);
     else if (N == -2) { printf("\nEncerrando...\n"); exit(0); }
     else if (N == 1)
       printf("\nVery funny.\n\n");
@@ -81,14 +86,14 @@ int main(void)
     {
       printf("\n-Fatores primos de N:\n\n");
 
-      clock_t begin = clock(); // tic...
-      fatoraInteiros(N);       // cool stuff
-      clock_t end = clock();   // ...toc
+      tic = clock();
+      fatoraInteiros(N);
+      tac = clock();
 
       if (numeroPrimo) { printf("--> %ld é um número primo!\n", N); }
 
-      tempoExec = (double)(end - begin) / CLOCKS_PER_SEC;
-      printf("\n-Tempo de execução: %.2f\n\n", tempoExec);
+      tempoExec = (double)(tac - tic) / CLOCKS_PER_SEC;
+      printf("\n-Tempo de execução: %.5fs\n\n", tempoExec);
     }
   }
 
