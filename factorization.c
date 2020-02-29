3db10f5add8d3eda50c7b32938c197ea6dc8bc01
alan-va <github@abrahao.proinbox.com>
2020-02-29 19:39:40 -0300

    Deixando o programa mais simplificado
    - Numero a ser fatorado é informado pela linha de comando.
    - Ao tentar fatorar 1, é retornado um link que explica
      porque 1 não é um número primo (nem composto).
    - Adicionada função usage(), mostra como usar o programa.
    - Em fatoraInteiros, ao buscar por fatores primos impares,
      o laço continua enquanto i <= floor(Num/2) E AuxN != 1,
      quando AuxN = 1, o numero foi completamente fatorado.
    - Nome do programa definido como "fatora".

--- factorization.c
+++ factorization.c
@@ -1,3 +1,5 @@
+#define nomePrograma "fatora"
+
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,27 +9,31 @@
 
 #define maiorInteiro LONG_MAX
 
-// variáveis globais
-long int AuxN;
-bool numeroPrimo;
+void usage(int status)
+{
+  printf("%s%s%s\n\n%s\n%s\n\n%s%s%s\n",
+    "Uso: ", nomePrograma, " [NUMERO]",
+    "Exibe cada fator primo de NUMERO junto com sua multiplicidade.",
+    "Para visualizar o maior inteiro representavel, use NUMERO = -1.",
+    "Exemplo: ", nomePrograma, " 29874636");
+  exit(status);
+} /* fim - usage */
 
 static int fatoraInteiros(long int Num)
 {
-  long int i, j; int k;
-  bool fatorPrimo = true;
+  bool fatorPrimo = true, numeroPrimo = true;
+  long int AuxN = Num; 
+  int k; size_t i, j;
 
   if (Num % 2 == 0 && Num != 2)
   {
     numeroPrimo = false; k = 0;
-    while (AuxN % 2 == 0) 
-    { 
-      k++; AuxN = AuxN / 2; 
-    }
+    while (AuxN % 2 == 0) { k++; AuxN = AuxN / 2; }
     printf(" 2^%d\n", k);
     if (AuxN == 1) { return 0; }
   }
-
-  for (i = 3; i <= floor(Num/2); i += 2)
+    
+  for (i = 3; AuxN != 1 && i <= floor(Num/2); i += 2)
   {
     if (AuxN % i == 0)
     {
@@ -35,66 +41,51 @@ static int fatoraInteiros(long int Num)
 
       for (j = 3; j <= floor(sqrt(i)); j += 2)
       {
-        if (i % j == 0)
-        {
-          fatorPrimo = false; break;
-        }
+        if (i % j == 0) { fatorPrimo = false; break; }
       }
 
       if (fatorPrimo)
       {
         k = 0;
-        while (AuxN % i == 0)
-        {
-          k++; AuxN = AuxN / i;
-        }
+        while (AuxN % i == 0) { k++; AuxN = AuxN / i; }
         printf(" %ld^%d\n", i, k);
-        if (AuxN == 1) { break; }
       }
     }
   }
+  
+  if (numeroPrimo)
+    printf("--> %ld é um número primo!\n", Num);
 
   return 0;
 } /* fim - fatoraInteiros */
 
-int main(void)
+int main(const int argc, char * const argv[])
 {
-  long int N;
-  float tempoExec;
+  long int N = 0;
+  float tempoExec = 0.0;
   clock_t tic, tac;
-
-  printf("%s\n%s\n\n%s\n%s\n\n%s\n\n", 
-    "Decomposição de inteiros em fatores primos",
-    "Dado N, este programa determina:",
-  "Cada fator pi (i=1, ... , n) de N e ki (multiplicidade do fator pi), i.e.,",
-  "p1^k1, p2^k2, ... , pn^kn tais que N = (p1^k1) * (p2^k2) * ... * (pn^kn).",
-  "[-1] Maior inteiro representável | [-2] Sair do programa");
   
-  while (true)
+  if (argc != 2)
+    usage(EXIT_SUCCESS);
+  else 
+    N = atol(argv[1]);
+
+  if (N == -1)
+    printf("Maior inteiro representável: %ld\n", maiorInteiro);
+  else if (N == 1)
+    printf("--> https://primes.utm.edu/notes/faq/one.html\n");
+  else if (N <= 0)
+    printf("São válidos apenas valores positivos.\n");
+  else
   {
-    printf("<Insira N> "); scanf("%ld", &N);
-    AuxN = N; numeroPrimo = true;
+    printf("-Fatores primos de %ld:\n\n", N);
 
-    if (N == -1)
-      printf("Maior inteiro representável: %ld\n\n", maiorInteiro);
-    else if (N == -2) { printf("\nEncerrando...\n"); exit(0); }
-    else if (N == 1)
-      printf("\nVery funny.\n\n");
-    else if (N <= 0)
-      printf("\nSão válidos apenas valores positivos.\n\n");
-    else
-    {
-      printf("\n-Fatores primos de N:\n\n");
+    tic = clock();
+    fatoraInteiros(N);
+    tac = clock();
 
-      tic = clock();
-      fatoraInteiros(N);
-      tac = clock();
-
-      if (numeroPrimo) { printf("--> %ld é um número primo!\n", N); }
-
-      tempoExec = (double)(tac - tic) / CLOCKS_PER_SEC;
-      printf("\n-Tempo de execução: %.5fs\n\n", tempoExec);
-    }
+    tempoExec = (double)(tac - tic) / CLOCKS_PER_SEC;
+    printf("\n-Tempo de execução: %.5fs\n", tempoExec);
   }
 
   return 0;
